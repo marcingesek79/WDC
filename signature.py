cbd0fe56a5989e8a63ecb8eef616458feb53346b
unknown <marcin.gesek79@gmail.com>
2022-05-13 22:44:09 +0200

    Add signature verification

--- signature.py
+++ signature.py
@@ -19,5 +19,33 @@ def generate_signature(key, data, signature_file):
     hash = SHA256.new(data)
     sender_private_key = RSA.import_key(key)
     signature = PKCS1_v1_5.new(sender_private_key).sign(hash)
-    with open(signature_file, "wb") as f: f.write(signature)
+    file = open(signature_file, "wb")
+    file.write(signature)
+    file.close()
+
+
+"""
+    Function that verifies digital signature.
+
+    INPUT:
+    - key - sender's public key
+    - data - signature data
+    - signature_file - file that contains signature
+
+    OUTPUT:
+    returns 'true' if signature is verified correctly
+    otherwise returns 'false'
+
+"""
+
+def verify_signature(key, data, signature_file):
+    hash = SHA256.new(data)
+    sender_public_key = RSA.import_key(key)
+    signer = PKCS1_v1_5.new(sender_public_key)
+    file = open(signature_file, "rb")
+    signature = file.read()
     
+    if (signer.verify(hash, signature)):
+        return True
+    else:
+        return False
