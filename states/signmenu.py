e18f545c9598c48ec24b329384740b4b0e51d6ab
jan-Kulpas <krzychu3813@gmail.com>
2022-05-14 21:07:03 +0200

    File selection

--- states/signmenu.py
+++ states/signmenu.py
@@ -1,15 +1,44 @@
 import pygame
+import tkinter
+import tkinter.filedialog
+
 from .base import BaseState
 from things.button import Button
 import things.colors as Colors
+from things.helpers import draw_text
+
+def do_nothing(menu):
+    pass
+
+def prompt_file():
+    """Create a Tk file dialog and cleanup when finished"""
+    top = tkinter.Tk()
+    top.withdraw()  # hide window
+    file_name = tkinter.filedialog.askopenfilename(parent=top)
+    top.destroy()
+    return file_name
+
+def choose_file(menu):
+    if menu.file_cooldown > 0:
+        return
+    menu.file_cooldown = 1
+    menu.chosen_file = prompt_file()
 
 class SignMenu(BaseState):
     def __init__(self):
         super(SignMenu, self).__init__()
         self.font = pygame.font.SysFont("urwgothic", 24)
 
-        bx = Button(Colors.BTN_BASE, Colors.BTN_HOV, self.font, "X", (850,50,80,40), self.switch_to("FIRSTMENU"))
-        self.buttons = [bx]
+        bx = Button(Colors.BTN_BASE, Colors.BTN_HOV, self.font, "X", (870,40,40,40), self.switch_to("FIRSTMENU"))
+        b1 = Button(Colors.BTN_BASE, Colors.BTN_HOV, self.font, "Wybierz plik", (300,250,180,40), choose_file)
+        b2 = Button(Colors.BTN_BASE, Colors.BTN_HOV, self.font, "Wybierz klucz", (300,490,180,40), do_nothing)
+        b3 = Button(Colors.BTN_BASE, Colors.BTN_HOV, self.font, "Wygeneruj klucz", (60,490,210,40), do_nothing)
+        b4 = Button(Colors.BTN_BASE, Colors.BTN_HOV, self.font, "Podpisz", (600,490,240,40), do_nothing)
+        self.buttons = [bx,b1,b2,b3,b4]
+
+        self.chosen_file = None
+        self.file_cooldown = 0
+        self.chosen_key = None
 
         self.next_state = None
 
@@ -30,8 +59,22 @@ class SignMenu(BaseState):
                 button.hover = True
             else:
                 button.hover = False
+                if button.click == choose_file:
+                    self.file_cooldown = 0
 
     def draw(self, surface):
         surface.fill(Colors.L_GREEN)
         for button in self.buttons:
             button.draw(surface)
+
+        filebox = pygame.Rect(60,60,420,180)
+        keybox = pygame.Rect(60,300,420,180)
+
+        pygame.draw.rect(surface, Colors.D_GREEN, filebox)
+        pygame.draw.rect(surface, Colors.D_GREEN, keybox)
+
+        draw_text(surface, self.chosen_file if self.chosen_file else "Nie wybrano pliku", self.font, Colors.BLACK, filebox)
+        draw_text(surface, self.chosen_key if self.chosen_key else "Nie wybrano klucza", self.font, Colors.BLACK, keybox)
+
+        draw_text(surface, "Plik:", self.font, Colors.BLACK, pygame.Rect(60,60,100,50))
+        draw_text(surface, "Klucz:", self.font, Colors.BLACK, pygame.Rect(60,300,100,50))
--- things/helpers.py
+++ things/helpers.py
@@ -1,1 +1,7 @@
 #Might be useful later
+import pygame
+
+def draw_text(surface, text, font, color, rect):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect(center=rect.center);
+    surface.blit(textobj, textrect)
