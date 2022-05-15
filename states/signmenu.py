15cc16c9049af3d6098724e5b2b95410af4c34f9
unknown <marcin.gesek79@gmail.com>
2022-05-15 17:47:25 +0200

    Added key selection

--- states/signmenu.py
+++ states/signmenu.py
@@ -6,6 +6,7 @@ from .base import BaseState
 from things.button import Button
 import things.colors as Colors
 from things.helpers import draw_text
+import key_generation as kg
 
 def do_nothing(menu):
     pass
@@ -27,6 +28,25 @@ def choose_file(menu):
     menu.file_cooldown = 1
     menu.chosen_file = prompt_file()
 
+def prompt_key():
+    top = tkinter.Tk()
+    top.withdraw()
+    key_name = tkinter.filedialog.askopenfilename(parent=top)
+
+    for format in kg.FORMATS:
+        if key_name.endswith(format):
+            top.destroy()
+            return key_name
+    
+    top.destroy()
+    return None
+
+def choose_key(menu):
+    if menu.file_cooldown > 0:
+        return
+    menu.file_cooldown = 1
+    menu.chosen_key = prompt_key()
+
 class SignMenu(BaseState):
     def __init__(self):
         super(SignMenu, self).__init__()
@@ -34,8 +54,8 @@ class SignMenu(BaseState):
 
         bx = Button(Colors.BTN_BASE, Colors.BTN_HOV, self.font, "X", (870,40,40,40), self.switch_to("FIRSTMENU"))
         b1 = Button(Colors.BTN_BASE, Colors.BTN_HOV, self.font, "Wybierz plik", (300,250,180,40), choose_file)
-        b2 = Button(Colors.BTN_BASE, Colors.BTN_HOV, self.font, "Wybierz klucz", (300,490,180,40), do_nothing)
-        b3 = Button(Colors.BTN_BASE, Colors.BTN_HOV, self.font, "Wygeneruj klucz", (60,490,210,40), self.switch_to("GENERATIONMENU"))
+        b2 = Button(Colors.BTN_BASE, Colors.BTN_HOV, self.font, "Wybierz klucz", (300,490,180,40), choose_key)
+        b3 = Button(Colors.BTN_BASE, Colors.BTN_HOV, self.font, "Wygeneruj klucze", (60,490,210,40), self.switch_to("GENERATIONMENU"))
         b4 = Button(Colors.BTN_BASE, Colors.BTN_HOV, self.font, "Podpisz", (600,490,240,40), do_nothing)
         self.buttons = [bx,b1,b2,b3,b4]
 
@@ -54,6 +74,9 @@ class SignMenu(BaseState):
         if event.type == pygame.MOUSEBUTTONDOWN:
             for button in self.buttons:
                 if button.hover:
+                    if button.text == "X" or button.text == "Wygeneruj klucz":
+                        self.chosen_file = None
+                        self.chosen_key = None
                     button.click(self)
 
     def update(self, dt):
@@ -81,4 +104,4 @@ class SignMenu(BaseState):
         draw_text(surface, self.chosen_key if self.chosen_key else "Nie wybrano klucza", self.font, Colors.BLACK, keybox)
 
         draw_text(surface, "Plik:", self.font, Colors.BLACK, pygame.Rect(60,60,100,50))
-        draw_text(surface, "Klucz:", self.font, Colors.BLACK, pygame.Rect(60,300,100,50))
+        draw_text(surface, "Prywatny klucz:", self.font, Colors.BLACK, pygame.Rect(80,300,100,50))
